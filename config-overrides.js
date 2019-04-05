a77d90a3acfe84878d779376346ad4daf4c65539
Odin Standal <odins@mnemonic.no>
2019-04-05 14:14:31 +0200

    Upgrade react-scripts to 2.1.8 (see create-react-app)
    - bump react deps
    - remove react-app-rewired as it does not support react-scripts 2.x
    - remove react-redux as it is no longer in use
    - removed semistandard as it conflicted with eslint from react-scripts

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const rewireMobX = require('react-app-rewire-mobx');
-const { injectBabelPlugin } = require('react-app-rewired');
-
-module.exports = function override (config, env) {
-  config = injectBabelPlugin('transform-class-properties', config);
-  config = rewireMobX(config, env);
-  return config;
-};

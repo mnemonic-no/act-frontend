2f797a2bc1114304a6348f5eb9d3bf05b4c9468b
Odin Standal <odins@mnemonic.no>
2019-05-13 15:26:53 +0200

    Show warning when result set is too large

--- src/pages/GraphView/GraphViewStore.ts
+++ src/pages/GraphView/GraphViewStore.ts
@@ -4,37 +4,51 @@ import getStyle from '../../core/cytoscapeStyle';
 import {ActObject, Search} from "../QueryHistory";
 
 export type Node = {
-    id: string|null
-    type: "fact"| "object"
+    id: string | null
+    type: "fact" | "object"
 }
 
 class GraphViewStore {
 
     root: MainPageStore;
 
+    renderThreshold: number = 1000;
+
     @observable selectedNode: Node = {type: "fact", id: null};
+    @observable acceptRenderWarning = false;
 
     constructor(root: MainPageStore) {
         this.root = root;
     }
 
-    @computed get prepared() {
+    @action
+    acceptRenderWarningOnClick() {
+        this.acceptRenderWarning = true;
+    }
+
+    @computed
+    get prepared() {
+        const canRender = this.acceptRenderWarning || this.root.refineryStore.cytoscapeElements.length < this.renderThreshold;
+
         return {
+            canRender: canRender,
             selectedNode: this.selectedNode.id,
             elements: this.root.refineryStore.cytoscapeElements,
             layout: this.root.ui.cytoscapeLayoutStore.graphOptions.layout.layoutObject,
-            style: getStyle({ showEdgeLabels: this.root.ui.cytoscapeLayoutStore.graphOptions.showFactEdgeLabels }),
-            onNodeClick: (node : any) => {
+            style: getStyle({showEdgeLabels: this.root.ui.cytoscapeLayoutStore.graphOptions.showFactEdgeLabels}),
+            onNodeClick: (node: any) => {
                 this.setSelectedNode({
                     id: node.data('isFact') ? node.data('factId') : node.id(),
-                    type: node.data('isFact') ? 'fact' : 'object'});
+                    type: node.data('isFact') ? 'fact' : 'object'
+                });
             },
-            onNodeCtxClick: (node : any) => {
+            onNodeCtxClick: (node: any) => {
                 this.setSelectedNode({
                     id: node.data('isFact') ? node.data('factId') : node.id(),
-                    type: node.data('isFact') ? 'fact' : 'object'})
+                    type: node.data('isFact') ? 'fact' : 'object'
+                })
             },
-            onNodeDoubleClick: (node : any) => {
+            onNodeDoubleClick: (node: any) => {
                 if (node.data('isFact')) return;
 
                 this.root.backendStore.executeQuery({
@@ -56,7 +70,7 @@ class GraphViewStore {
         // Select the searched object (can't do that on graph queries)
         if (!search.query) {
             const searchedNode = Object.values(this.root.refineryStore.refined.objects)
-                .find( (object: ActObject) => object.type.name === search.objectType &&
+                .find((object: ActObject) => object.type.name === search.objectType &&
                     object.value === search.objectValue);
             if (searchedNode) {
                 this.root.ui.cytoscapeStore.setSelectedNode({id: searchedNode.id, type: 'object'})

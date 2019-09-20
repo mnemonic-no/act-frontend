5a4ee1cf1dde176ee5ef4f41c8ef6b42b4f715a6
Odin Standal <odins@mnemonic.no>
2019-09-20 11:49:57 +0200

    Search for objects based on fact name

--- src/core/dataLoaders.ts
+++ src/core/dataLoaders.ts
@@ -292,7 +292,7 @@ export const postJson = (url: string, jsonRequest: any) => {
     .catch(handleError);
 };
 
-export const objectSearch = (keywords: string): Promise<Array<ActObject>> => {
+export const objectKeywordSearch = (keywords: string): Promise<Array<ActObject>> => {
   const requestBody = {
     keywords: keywords,
     limit: 300
@@ -300,6 +300,21 @@ export const objectSearch = (keywords: string): Promise<Array<ActObject>> => {
 
   return actWretch
     .url('/v1/object/search')
+    .json(requestBody)
+    .post()
+    .json(({ data }: any) => data)
+    .catch(handleError);
+};
+
+export const factKeywordSearch = (keywords: string): Promise<Array<ActObject>> => {
+  const requestBody = {
+    limit: 300,
+    keywords: keywords,
+    factType: ['name']
+  };
+
+  return actWretch
+    .url('/v1/fact/search')
     .json(requestBody)
     .post()
     .json(({ data }: any) => data)
--- src/pages/SimpleSearchBackendStore.ts
+++ src/pages/SimpleSearchBackendStore.ts
@@ -1,5 +1,9 @@
 import { action, computed, observable } from 'mobx';
-import { objectSearch } from '../core/dataLoaders';
+import * as _ from 'lodash/fp';
+
+import { factKeywordSearch, objectKeywordSearch } from '../core/dataLoaders';
+import { factsToObjects } from '../core/transformers';
+import { ActObject } from './types';
 
 type SimpleSearch = {
   searchString: string;
@@ -25,7 +29,15 @@ class SimpleSearchBackendStore {
     this.searches[searchString] = search;
 
     try {
-      const result = await objectSearch(searchString);
+      const [objectsByValue, factsByNameResult] = await Promise.all([
+        objectKeywordSearch(searchString),
+        factKeywordSearch(searchString)
+      ]);
+
+      const objectsFromFacts = factsToObjects(factsByNameResult);
+
+      const result = _.uniqBy((x: ActObject) => x.id)([...objectsByValue, ...objectsFromFacts]);
+
       this.searches[searchString] = { ...search, status: 'done', result: result };
     } catch (err) {
       this.searches[searchString] = { ...search, status: 'rejected' };
